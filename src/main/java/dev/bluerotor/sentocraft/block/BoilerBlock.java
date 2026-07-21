72b6330fe39fffc170e0882b7de65d64199f56f8
BlueRotor <tyuusann12114@gmail.com>
2026-07-21 22:27:58 +0900

    Add directional boiler block

--- src/main/java/dev/bluerotor/sentocraft/block/BoilerBlock.java
+++ src/main/java/dev/bluerotor/sentocraft/block/BoilerBlock.java
@@ -3,11 +3,18 @@ package dev.bluerotor.sentocraft.block;
 import com.mojang.serialization.MapCodec;
 import dev.bluerotor.sentocraft.blockentity.BoilerBlockEntity;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.block.BaseEntityBlock;
+import net.minecraft.world.level.block.HorizontalDirectionalBlock;
+import net.minecraft.world.level.block.Mirror;
 import net.minecraft.world.level.block.RenderShape;
+import net.minecraft.world.level.block.Rotation;
 import net.minecraft.world.level.block.entity.BlockEntity;
-import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.DirectionProperty;
 import org.jetbrains.annotations.Nullable;
 
 public class BoilerBlock extends BaseEntityBlock {
@@ -15,8 +22,16 @@ public class BoilerBlock extends BaseEntityBlock {
     public static final MapCodec<BoilerBlock> CODEC =
             simpleCodec(BoilerBlock::new);
 
+    public static final DirectionProperty FACING =
+            HorizontalDirectionalBlock.FACING;
+
     public BoilerBlock(BlockBehaviour.Properties properties) {
         super(properties);
+
+        this.registerDefaultState(
+                this.stateDefinition.any()
+                        .setValue(FACING, Direction.NORTH)
+        );
     }
 
     @Override
@@ -33,5 +48,26 @@ public class BoilerBlock extends BaseEntityBlock {
     @Override
     public BlockEntity newBlockEntity(BlockPos pos, BlockState state) {
         return new BoilerBlockEntity(pos, state);
+    }
+
+    @Override
+    public BlockState getStateForPlacement(BlockPlaceContext context) {
+        return defaultBlockState()
+                .setValue(FACING, context.getHorizontalDirection().getOpposite());
+    }
+
+    @Override
+    protected BlockState rotate(BlockState state, Rotation rotation) {
+        return state.setValue(FACING, rotation.rotate(state.getValue(FACING)));
+    }
+
+    @Override
+    protected BlockState mirror(BlockState state, Mirror mirror) {
+        return state.rotate(mirror.getRotation(state.getValue(FACING)));
+    }
+
+    @Override
+    protected void createBlockStateDefinition(StateDefinition.Builder<net.minecraft.world.level.block.Block, BlockState> builder) {
+        builder.add(FACING);
     }
 }
